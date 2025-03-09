149172f40c572542757bcc0a50f2277780c52abe
Srijamotegit <srijasrija123678@gmail.com>
2025-03-09 16:51:13 +0530

    push project

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
 mongoose.connect(
-  "mongodb+srv://demomongo123:demomongo123@cluster0.bwegp.mongodb.net/recipetest?retryWrites=true&w=majority&appName=Cluster0",
+  "mongodb://localhost:27017/",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
